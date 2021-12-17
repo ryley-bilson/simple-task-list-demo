b8de80f9ffd0314d9532a064cf796ce5fcccd957
ryley.bilson <ryley.bilson@gmail.com>
2021-12-17 10:58:28 +0800

    Initial Upload

---
+++ AT3/ProtectedFileObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AT3
+{
+    [Serializable()]
+    public class ProtectedFileObject
+    {
+        private Object fileData;
+        private byte[] hash;
+
+        public ProtectedFileObject(Object inFD, byte[] inHash)
+        {
+            fileData = inFD;
+            hash = inHash;
+        }
+        public Object FileData
+        {
+            get => fileData;
+        }
+        public byte[] Hash
+        {
+            get => hash;
+        }
+    }
+}
---
+++ BinReadWrite/BinReadWrite.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinReadWrite
+{
+    public class BinReadWrite
+    {
+        private String filepath;
+        private Object fileData;
+
+        public BinReadWrite()
+        {
+            filepath = "";
+        }
+        public BinReadWrite(String inFP, Object inFD)
+        {
+            filepath = inFP;
+            fileData = inFD;
+        }
+        public String Filepath
+        {
+            get => filepath;
+            // Directory must exist, file doesn't need to.
+            set => filepath = (Directory.Exists(Path.GetDirectoryName(value))) ? value : filepath;
+        }
+        public Object FileData
+        {
+            get => fileData;
+            set => fileData = (value != null) ? value : fileData;
+        }
+        public bool readFile()
+        {
+            fileData = staticReadFile(filepath);
+            return (FileData != null);
+        }
+        public bool writeFile()
+        {
+            return staticWriteFile(filepath, fileData);
+        }
+        public static Object staticReadFile(String inFP)
+        {
+            Object loadData = null;
+
+            try
+            {
+                Stream stream = File.Open(inFP, FileMode.OpenOrCreate);
+                BinaryFormatter bf = new BinaryFormatter();
+                loadData = bf.Deserialize(stream);
+
+                stream.Close();
+            }
+            catch (IOException ex) { }
+
+            return loadData;
+        }
+        public static bool staticWriteFile(String inFP, Object inFD)
+        {
+            bool isSuccess = false;
+
+            try
+            {
+                Stream stream = File.Open(inFP, FileMode.OpenOrCreate);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(stream, inFD);
+
+                stream.Close();
+                isSuccess = true;
+            }
+            catch (IOException ex) { }
+
+            return isSuccess;
+        }
+    }
+}
